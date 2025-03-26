e1b8a4f3c38c611909dea6f2650eb329f5607a68
Hammad Qayyoom <hammadqayyoom0@gmail.com>
2025-03-26 23:29:24 +0500

    Updated feature X or Fixed issue Y

--- lib/screens/auth-ui/sign-in-screen.dart
+++ lib/screens/auth-ui/sign-in-screen.dart
@@ -1,25 +1,31 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:get/get.dart';
+import 'package:greenage_ecomm/controllers/sign-in-controller.dart';
 import 'package:lottie/lottie.dart';
 import '../../utils/app-constant.dart';
+import '../user-panel/main-screen.dart';
 import 'sign-up-screen.dart';
 
 class SignInScreen extends StatefulWidget {
-  const SignInScreen({super.key});
+  const SignInScreen({Key? key}) : super(key: key);
 
   @override
   State<SignInScreen> createState() => _SignInScreenState();
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final SignInController signInController = Get.put(SignInController());
+  TextEditingController userEmail = TextEditingController();
+  TextEditingController userPassword = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return KeyboardVisibilityBuilder(
       builder: (context, isKeyboardVisible) {
-        // Fixed: Added builder function
         return Scaffold(
           appBar: AppBar(
             backgroundColor: AppConstant.appSecondaryColor,
@@ -36,35 +42,33 @@ class _SignInScreenState extends State<SignInScreen> {
                   isKeyboardVisible
                       ? SizedBox.shrink()
                       : Column(
-                          children: [
-                            Lottie.asset('assets/images/splash-icon.json'),
-                          ],
-                        ),
-                  SizedBox(
-                    height: Get.height / 20,
+                    children: [
+                      Lottie.asset('assets/images/splash-icon.json'),
+                    ],
                   ),
+                  SizedBox(height: Get.height / 20),
                   Container(
                     margin: EdgeInsets.symmetric(horizontal: 5.0),
                     width: Get.width,
                     child: Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: TextField(
-                          cursorColor: AppConstant.appSecondaryColor,
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(
-                              Icons.email,
-                              color: AppConstant.appSecondaryColor,
-                            ),
-                            contentPadding:
-                                EdgeInsets.only(top: 10.0, left: 10.0),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            hintText: 'Email',
-                            hintStyle:
-                                TextStyle(color: AppConstant.appSecondaryColor),
-                          )),
+                        controller: userEmail,
+                        cursorColor: AppConstant.appSecondaryColor,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.email,
+                            color: AppConstant.appSecondaryColor,
+                          ),
+                          contentPadding: EdgeInsets.only(top: 10.0, left: 10.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          hintText: 'Email',
+                          hintStyle: TextStyle(color: AppConstant.appSecondaryColor),
+                        ),
+                      ),
                     ),
                   ),
                   Container(
@@ -72,7 +76,10 @@ class _SignInScreenState extends State<SignInScreen> {
                     width: Get.width,
                     child: Padding(
                       padding: const EdgeInsets.all(10.0),
-                      child: TextField(
+                      child: Obx(
+                            () => TextField(
+                          controller: userPassword,
+                          obscureText: signInController.isPasswordVisible.value,
                           cursorColor: AppConstant.appSecondaryColor,
                           keyboardType: TextInputType.visiblePassword,
                           decoration: InputDecoration(
@@ -80,16 +87,25 @@ class _SignInScreenState extends State<SignInScreen> {
                               Icons.password,
                               color: AppConstant.appSecondaryColor,
                             ),
-                            suffixIcon: Icon(Icons.visibility_off),
-                            contentPadding:
-                                EdgeInsets.only(top: 10.0, left: 10.0),
+                            suffixIcon: GestureDetector(
+                              onTap: () {
+                                signInController.isPasswordVisible.toggle();
+                              },
+                              child: Icon(
+                                signInController.isPasswordVisible.value
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                              ),
+                            ),
+                            contentPadding: EdgeInsets.only(top: 10.0, left: 10.0),
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10.0),
                             ),
                             hintText: 'Password',
-                            hintStyle:
-                                TextStyle(color: AppConstant.appSecondaryColor),
-                          )),
+                            hintStyle: TextStyle(color: AppConstant.appSecondaryColor),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                   Container(
@@ -98,13 +114,12 @@ class _SignInScreenState extends State<SignInScreen> {
                     child: Text(
                       "Forgot Password?",
                       style: TextStyle(
-                          color: AppConstant.appSecondaryColor,
-                          fontWeight: FontWeight.bold),
+                        color: AppConstant.appSecondaryColor,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                  SizedBox(
-                    height: Get.height / 20,
-                  ),
+                  SizedBox(height: Get.height / 20),
                   Material(
                     child: Container(
                       width: Get.width / 2,
@@ -114,8 +129,50 @@ class _SignInScreenState extends State<SignInScreen> {
                         borderRadius: BorderRadius.circular(20),
                       ),
                       child: TextButton.icon(
-                        onPressed: () {
-                          Get.toNamed('/LoginScreen');
+                        onPressed: () async {
+                          String email = userEmail.text.trim();
+                          String password = userPassword.text.trim();
+
+                          if (email.isEmpty || password.isEmpty) {
+                            Get.snackbar(
+                              "Error",
+                              "Please fill all the fields",
+                              snackPosition: SnackPosition.BOTTOM,
+                              backgroundColor: AppConstant.appSecondaryColor,
+                              colorText: AppConstant.appTextColor,
+                            );
+                          } else {
+                            UserCredential? userCredential =
+                            await signInController.signInMethod(email, password);
+                            if (userCredential != null) {
+                              if (userCredential.user!.emailVerified) {
+                                Get.snackbar(
+                                  "Success",
+                                  "Login Successfully",
+                                  snackPosition: SnackPosition.BOTTOM,
+                                  backgroundColor: AppConstant.appSecondaryColor,
+                                  colorText: AppConstant.appTextColor,
+                                );
+                                Get.offAll(() => MainScreen());
+                              } else {
+                                Get.snackbar(
+                                  "Error",
+                                  "Please verify your email",
+                                  snackPosition: SnackPosition.BOTTOM,
+                                  backgroundColor: AppConstant.appSecondaryColor,
+                                  colorText: AppConstant.appTextColor,
+                                );
+                              }
+                            } else {
+                              Get.snackbar(
+                                "Error",
+                                "Please Try Again",
+                                snackPosition: SnackPosition.BOTTOM,
+                                backgroundColor: AppConstant.appSecondaryColor,
+                                colorText: AppConstant.appTextColor,
+                              );
+                            }
+                          }
                         },
                         label: const Text(
                           "Sign In",
@@ -124,9 +181,7 @@ class _SignInScreenState extends State<SignInScreen> {
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: Get.height / 20,
-                  ),
+                  SizedBox(height: Get.height / 20),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -135,18 +190,19 @@ class _SignInScreenState extends State<SignInScreen> {
                         style: TextStyle(color: AppConstant.appSecondaryColor),
                       ),
                       GestureDetector(
-                          onTap: () {
-                            Get.offAll(SignUpScreen());
-                          },
-                          child: Text(
-
-                        " Sign Up" ,
-                        style: TextStyle(
+                        onTap: () {
+                          Get.offAll(SignUpScreen());
+                        },
+                        child: Text(
+                          " Sign Up",
+                          style: TextStyle(
                             color: AppConstant.appSecondaryColor,
-                            fontWeight: FontWeight.bold),
-                      ),),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
